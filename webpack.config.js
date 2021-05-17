dfb2b62a25bb55aa08b7d55d0e1dc9f67b1e13a2
Florian Kapaun <hello@florian-kapaun.de>
2021-05-17 14:57:41 +0200

    :wrench: Fix Babel Config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,24 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/ping.js'),
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', { targets: "defaults" }]
-            ]
-          }
-        }
-      }
-    ],
-  },
-  resolve: {
-    extensions: ['*', '.js'],
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'ping.min.js',
-  },
+    entry: path.resolve(__dirname, './src/ping.js'),
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['*', '.js'],
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'ping.min.js',
+    },
 };
